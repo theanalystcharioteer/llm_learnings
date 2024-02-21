d32b720b875598840a187c1cbddaf5cdd83f5a6c
Pinaki Brahma <42740963+theanalystcharioteer@users.noreply.github.com>
2024-02-21 07:26:37 +0530

    Update output_formats.py

--- output_formats.py
+++ output_formats.py
@@ -30,3 +30,42 @@ json_parser = SimpleJsonOutputParser()
 json_chain = json_prompt | model | json_parser
 
 list(json_chain.stream({"question": "Who invented the microscope?"}))
+
+# ================================================= 
+# https://python.langchain.com/docs/modules/model_io/output_parsers/types/json
+# using langchain
+# ================================================= 
+from typing import List
+
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(temperature=0)
+
+# Define your desired data structure.
+class Joke(BaseModel):
+    setup: str = Field(description="question to set up a joke")
+    punchline: str = Field(description="answer to resolve the joke")
+
+# And a query intented to prompt a language model to populate the data structure.
+joke_query = "Tell me a joke."
+
+# Set up a parser + inject instructions into the prompt template.
+parser = JsonOutputParser(pydantic_object=Joke)
+
+prompt = PromptTemplate(
+    template="Answer the user query.\n{format_instructions}\n{query}\n",
+    input_variables=["query"],
+    partial_variables={"format_instructions": parser.get_format_instructions()},
+)
+
+chain = prompt | model | parser
+
+chain.invoke({"query": joke_query})
+
+# output: 
+# {'setup': "Why don't scientists trust atoms?",
+#  'punchline': 'Because they make up everything!'}
+
